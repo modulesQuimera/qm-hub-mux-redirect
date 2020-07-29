36a59fd2f8ad1d5dfcc01756d685ce276d8234f6
Ian Alves <ian.alves21@gmail.com>
2020-07-29 18:49:41 -0400

    update

--- redirect.js
+++ redirect.js
@@ -1,14 +1,71 @@
 module.exports = function(RED) {
 
-    "use strict";
     var mapeamentoNode;
+
+    function multipleRedirect(self, file, slot, currentMode){
+        for(var t=0; t<self.qtdRedirect; t++){
+            var command_n={
+                type: "mux_modular_V1_0",
+                slot: parseInt(mapeamentoNode.slot),
+                method: "redirect",
+                channel_selector: parseInt(self.channel_selector_n[t]),
+                get_output: {},
+                compare: {}
+            }
+            if(!(slot === "begin" || slot === "end")){
+                if(currentMode == "test"){
+                    file.slots[slot].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[slot].jig_error.push(command_n);
+                }
+            }
+            else{
+                if(slot === "begin"){
+                    file.slots[0].jig_test.push(command_n);
+                }
+                else{
+                    file.slots[3].jig_test.push(command_n);
+                }
+            }
+        }
+        return file;
+    }
 
     function RedirectNode(config) {
         RED.nodes.createNode(this, config);
-        this.mapeamento = config.mapeamento
-        this.channel_selector = config.channel_selector
+        this.mapeamento = config.mapeamento;
+        this.channel_selector = config.channel_selector;
+
+        this.qtdRedirect = config.qtdRedirect;
+        this.channel_selector_n = [];
+        this.channel_selector_n.push(config.channel_selector1);
+        this.channel_selector_n.push(config.channel_selector2);
+        this.channel_selector_n.push(config.channel_selector3);
+        this.channel_selector_n.push(config.channel_selector4);
+        this.channel_selector_n.push(config.channel_selector5);
+        this.channel_selector_n.push(config.channel_selector6);
+        this.channel_selector_n.push(config.channel_selector7);
+        this.channel_selector_n.push(config.channel_selector8);
+        this.channel_selector_n.push(config.channel_selector9);
+        this.channel_selector_n.push(config.channel_selector10);
+        this.channel_selector_n.push(config.channel_selector11);
+        this.channel_selector_n.push(config.channel_selector12);
+        this.channel_selector_n.push(config.channel_selector13);
+        this.channel_selector_n.push(config.channel_selector14);
+        this.channel_selector_n.push(config.channel_selector15);
+        this.channel_selector_n.push(config.channel_selector16);
+        this.channel_selector_n.push(config.channel_selector17);
+        this.channel_selector_n.push(config.channel_selector18);
+        this.channel_selector_n.push(config.channel_selector19);
+        this.channel_selector_n.push(config.channel_selector20);
+        this.channel_selector_n.push(config.channel_selector21);
+        this.channel_selector_n.push(config.channel_selector22);
+        this.channel_selector_n.push(config.channel_selector23);
+        this.channel_selector_n.push(config.channel_selector24);
+
         mapeamentoNode = RED.nodes.getNode(this.mapeamento);
-        var node = this
+        var node = this;
         
         node.on('input', function(msg, send, done) {
             var globalContext = node.context().global;
@@ -22,77 +79,33 @@ module.exports = function(RED) {
                 channel_selector: parseInt(node.channel_selector),
                 get_output: {},
                 compare: {}
-            }
+            };
 
-            var file = globalContext.get("exportFile")
+            var file = globalContext.get("exportFile");
             if(!(slot === "begin" || slot === "end")){
                 if(currentMode == "test"){
                     file.slots[slot].jig_test.push(command);
+                    file = multipleRedirect(node, file, slot, currentMode);
                 }
                 else{
                     file.slots[slot].jig_error.push(command);
+                    file = multipleRedirect(node, file, slot, currentMode);
                 }
             }
             else{
                 if(slot === "begin"){
                     file.slots[0].jig_test.push(command);
-                    // file.begin.push(command);
+                    file = multipleRedirect(node, file, slot, currentMode);
                 }
                 else{
                     file.slots[3].jig_test.push(command);
-                    // file.end.push(command);
+                    file = multipleRedirect(node, file, slot, currentMode);
                 }
             }
             globalContext.set("exportFile", file);
-            // node.status({fill:"green", shape:"dot", text:"done"}); // seta o status pra waiting
-            // msg.payload = command
-            console.log(command)
-            send(msg)
+            console.log(command);
+            send(msg);
         });
     }
     RED.nodes.registerType("redirect", RedirectNode);
-
-    // RED.httpAdmin.get("/getMapeamentoMux",function(req,res) {
-    //     console.log(mapeamentoNode)
-    //     if(mapeamentoNode){
-    //         res.json([
-    //             {value:mapeamentoNode.valuePort1, label:"A01/B01 - " + mapeamentoNode.labelPort1, hasValue:false},
-    //             {value:mapeamentoNode.valuePort2, label: "A02/B02 - " + mapeamentoNode.labelPort2, hasValue:false},
-    //             {value:mapeamentoNode.valuePort3, label: "A03/B03 - " + mapeamentoNode.labelPort3, hasValue:false},
-    //             {value:mapeamentoNode.valuePort4, label: "A04/B04 - " + mapeamentoNode.labelPort4, hasValue:false},
-    //             {value:mapeamentoNode.valuePort5, label: "A05/B05 - " + mapeamentoNode.labelPort5, hasValue:false},
-    //             {value:mapeamentoNode.valuePort6, label: "A06/B06 - " + mapeamentoNode.labelPort6, hasValue:false},
-    //             {value:mapeamentoNode.valuePort7, label: "A07/B07 - " + mapeamentoNode.labelPort7, hasValue:false},
-    //             {value:mapeamentoNode.valuePort8, label: "A08/B08 - " + mapeamentoNode.labelPort8, hasValue:false},
-    //             {value:mapeamentoNode.valuePort9, label: "A09/B09 - " + mapeamentoNode.labelPort9, hasValue:false},
-    //             {value:mapeamentoNode.valuePort10, label: "A10/B10 - " + mapeamentoNode.labelPort10, hasValue:false},
-    //             {value:mapeamentoNode.valuePort11, label: "A11/B11 - " + mapeamentoNode.labelPort11, hasValue:false},
-    //             {value:mapeamentoNode.valuePort12, label: "A12/B12 - " + mapeamentoNode.labelPort12, hasValue:false},
-    //             {value:mapeamentoNode.valuePort13, label: "A13/B13 - " + mapeamentoNode.labelPort13, hasValue:false},
-    //             {value:mapeamentoNode.valuePort14, label: "A14/B14 - " + mapeamentoNode.labelPort14, hasValue:false},
-    //             {value:mapeamentoNode.valuePort15, label: "A15/B15 - " + mapeamentoNode.labelPort15, hasValue:false},
-    //             {value:mapeamentoNode.valuePort16, label: "A16/B16 - " + mapeamentoNode.labelPort16, hasValue:false},
-    //         ])
-    //     }
-    //     else{
-    //         res.json([
-    //             {label:"A01/B01 - ", value: "0", hasValue:false},
-    //             {label:"A02/B02 - ", value: "1", hasValue:false},
-    //             {label:"A03/B03 - ", value: "2", hasValue:false},
-    //             {label:"A04/B04 - ", value: "3", hasValue:false},
-    //             {label:"A05/B05 - ", value: "4", hasValue:false},
-    //             {label:"A06/B06 - ", value: "5", hasValue:false},
-    //             {label:"A07/B07 - ", value: "6", hasValue:false},
-    //             {label:"A08/B08 - ", value: "7", hasValue:false},
-    //             {label:"A09/B09 - ", value: "8", hasValue:false},
-    //             {label:"A10/B10 - ", value: "9", hasValue:false},
-    //             {label:"A11/B11 - ", value: "10", hasValue:false},
-    //             {label:"A12/B12 - ", value: "11", hasValue:false},
-    //             {label:"A13/B13 - ", value: "12", hasValue:false},
-    //             {label:"A14/B14 - ", value: "13", hasValue:false},
-    //             {label:"A15/B15 - ", value: "14", hasValue:false},
-    //             {label:"A16/B16 - ", value: "15", hasValue:false},
-    //         ])
-    //     }
-    // });
-}
+};
