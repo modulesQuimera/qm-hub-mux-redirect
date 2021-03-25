856453236cd21b25f04d6977907c70a116c70f43
SCROLLUCK <llc2@icomp.ufam.edu.br>
2021-03-24 23:11:43 -0400

    Update ports mapping, changed mapping used to (mux-mapping) and remove console.log bad pratice

--- redirect.js
+++ redirect.js
@@ -103,7 +103,7 @@ module.exports = function(RED) {
                 }
             }
             globalContext.set("exportFile", file);
-            console.log(command);
+            
             send(msg);
         });
     }
